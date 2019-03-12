2b8e9b433e7148fcda6b3a0a6d05ec3d49d4bd4e
Gpe. Josue Uribe <guadalupe.uribe.dse@uteq.edu.mx>
2019-03-11 21:53:55 -0600

    pwm_thread_entry.c
    1.- Eliminate magic numbers.
    2.- Invert duty cycle to properly show the result on the GUI.

--- M4_DC_MVC/src/pwm_thread_entry.c
+++ M4_DC_MVC/src/pwm_thread_entry.c
@@ -10,9 +10,10 @@
 #define CLOCK_B                     1
 #define DEFAULT_DUTY_CYCLE          5
 #define DEFAULT_ADC_VALUE         512
-#define DELTA_DUTY                  5
 #define PWM_THREAD_SLEEP            2 /* Milliseconds to sleep */
-
+#define MAX_DUTY_CYCLE            100
+#define TEN_BIT_RESOLUTION       1023
+#define TRACE_BUFFER_SIZE         100
 
 /*!===========================================================================*
  * External Type Declarations
@@ -36,8 +37,12 @@ const sf_message_post_cfg_t g_post_cfg =
  * Local Type Declarations
  *===========================================================================*/
 static uint8_t duty_cycle = DEFAULT_DUTY_CYCLE;
+static uint8_t inverted_duty_cycle = DEFAULT_DUTY_CYCLE;
 static uint16_t received_adc_value = DEFAULT_ADC_VALUE; /* to store queued value from ADC thread */
-static char send_trace[100]; /* to store debug strings */
+static char send_trace[TRACE_BUFFER_SIZE]; /* to store debug strings */
+/* Pointers to event message and data */
+static sf_message_header_t *p_message  = NULL;
+static sf_message_payload_t *p_payload = NULL;
 
 UINT tx_status_pwm = TX_SUCCESS;
 
@@ -51,24 +56,21 @@ void pwm_thread_entry(void)
 
     while (1)
     {
-        tx_status_pwm = tx_queue_receive(&g_cdc_to_pwm_queue, &received_adc_value, TX_WAIT_FOREVER);  //receive adc values from adc_thread_entry
+       tx_status_pwm = tx_queue_receive(&g_cdc_to_pwm_queue, &received_adc_value, TX_WAIT_FOREVER);  /* receive adc raw data from adc_thread_entry */
        if (TX_SUCCESS != tx_status_pwm)
        {
            sprintf(send_trace, "Error %d receiving data from queue\r", tx_status_pwm);
            tx_queue_send(&g_cdc_queue, send_trace, TX_NO_WAIT);
        }
 
-       duty_cycle = (uint8_t)(received_adc_value * 100 / 1023);   //convert adc value to percentage value
-
+       duty_cycle = (uint8_t)(received_adc_value * MAX_DUTY_CYCLE / TEN_BIT_RESOLUTION); /* convert adc value to percentage value */
+       inverted_duty_cycle = MAX_DUTY_CYCLE - duty_cycle; /* Invert duty cycle value to appropriately show percentage on the GUI */
        g_timer0.p_api->dutyCycleSet(g_timer0.p_ctrl, duty_cycle, TIMER_PWM_UNIT_PERCENT, CLOCK_B);
 
        /*Console traces*/
        sprintf(send_trace, "Duty Cycle: %d\r", duty_cycle);
        tx_queue_send(&g_cdc_queue, send_trace, TX_NO_WAIT);
 
-       /* Pointers to event message and data */
-       sf_message_header_t *p_message  = NULL;
-       sf_message_payload_t *p_payload = NULL;
        /* Get buffer from Messaging framework. */
        ssp_err_t err = g_sf_message0.p_api->bufferAcquire(g_sf_message0.p_ctrl, (sf_message_header_t **) &p_message, &g_acquire_cfg, TX_NO_WAIT);
 
@@ -82,7 +84,7 @@ void pwm_thread_entry(void)
            p_message->event_b.class_code = SF_MESSAGE_EVENT_CLASS_PWM_DATA;
            p_message->event_b.code  = SF_MESSAGE_EVENT_UPDATE_DUTY_CYCLE;
            p_payload = (sf_message_payload_t*)(p_message+1);
-           p_payload->pwm_data_payload.Duty_Cycle = duty_cycle;
+           p_payload->pwm_data_payload.Duty_Cycle = inverted_duty_cycle;
 
            /* Posting message. */
            sf_message_post_err_t post_err;
@@ -118,4 +120,8 @@ void pwm_thread_entry(void)
  *
  * - 03-Mar-2019 Gpe. Josue Uribe  Rev 5
  *   - Task: Include pointers to Event Message and data in the infinite loop.
+ *
+ * - 11-Mar-2019 Gpe. Josue Uribe  Rev 6
+ *   - Task: Eliminate magic numbers and invert duty cycle value to properly show the result on the GUI.
+ *
  *===========================================================================*/
